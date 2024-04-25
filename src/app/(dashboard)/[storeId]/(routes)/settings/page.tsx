5c4ad9d162d7d90d220ccb2cb044a6fbcefd46f3
BarraR3port <barrar3port@gmail.com>
2024-04-24 23:27:13 -0400

    chore: Corregir estilos en la página de configuración

--- src/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -28,7 +28,7 @@ export default async function SettingsPage({ params }: SettingsPageProps) {
 
 	return (
 		<div className="flex-col">
-			<div className="flex-1 space-y-4 p-8 pt-6">
+			<div className="flex-1 space-y-4 p-5 pt-6">
 				<SettingsForm store={store} />
 			</div>
 		</div>
